9a3f8d159fea47f0ec6df0a3cf991291ed272d9a
geraskalnas <geraskalnas@gmail.com>
2021-04-12 19:59:56 +0300

    left and right

--- py3-auto-clicker.py
+++ py3-auto-clicker.py
@@ -14,7 +14,7 @@ cps = 0
 ###speed = 1/20
 minCPS = 20
 maxCPS = 30
-combinations=1000
+combinations=10000
 right=True
 
 def getSpeed():
@@ -33,37 +33,29 @@ def counter():
         cps = 0
         time.sleep(1)
 
-def clicker():
+def clickerRight():
     global cps
     #global speed
 
     while True:
-        #while win32api.GetAsyncKeyState(win32con.VK_MENU) != 0 and win32api.GetAsyncKeyState(87) != 0: #alt+x
         while win32api.GetAsyncKeyState(win32con.VK_MENU) != 0 and win32api.GetAsyncKeyState(90) != 0: #alt+z
                 (x,y) = win32api.GetCursorPos() #Get the current position of the cursor
-                if(right):
-                    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
-                    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
-                else:
-                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
                 cps += 1
                 time.sleep(getSpeed())
-                '''                
-                if win32api.GetAsyncKeyState(38) != 0 and win32api.GetAsyncKeyState(16) != 0 and speed > 0.005: #Check if Shift + Up is pressed
-                    speed -= 0.005
-                    cps = 0
-                    time.sleep(0.1)
 
-                if win32api.GetAsyncKeyState(40) != 0 and win32api.GetAsyncKeyState(16) != 0 and speed < 1: #Check if Shift + Down is pressed
-                    speed += 0.005
-                    cps = 0
-                    time.sleep(0.1)
-                
-                if win32api.GetAsyncKeyState(win32con.VK_MENU) != 0 and win32api.GetAsyncKeyState(90) != 0: #Check if Shift + W is pressed
-                    break
-                    cps = 0
-                '''
+def clickerLeft():
+    global cps
+    #global speed
+
+    while True:
+        while win32api.GetAsyncKeyState(win32con.VK_MENU) != 0 and win32api.GetAsyncKeyState(88) != 0: #alt+x
+                (x,y) = win32api.GetCursorPos() #Get the current position of the cursor
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+                cps += 1
+                time.sleep(getSpeed())
 
 print("Generating.")
 
@@ -73,15 +65,19 @@ for i in range(combinations):
 
 print("Starting services...")
 
-t1 = Thread(target = clicker) #If threads were not daemons the program would not exit with keyboard interrupt
+t1 = Thread(target = clickerRight) #If threads were not daemons the program would not exit with keyboard interrupt
 t1.daemon = True
 t1.start()
 
-print("Ready.\n\n")
-
-t2 = Thread(target = counter)
+t2 = Thread(target = clickerLeft)
 t2.daemon = True
 t2.start()
+
+print("Ready.\n\n")
+
+t3 = Thread(target = counter)
+t3.daemon = True
+t3.start()
 
 
 
